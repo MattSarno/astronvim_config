92773450200db19908f0afe2e5982db1a25b8890
Matt Sarno <matt.sarno@pm.me>
2024-05-31 20:56:36 -0600

    feat: add custom markdown package

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "markdown", "markdown_inline" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "marksman" })
+    end,
+  },
+  {
+    "ellisonleao/glow.nvim", 
+    config = true, 
+    cmd = "Glow"
+  },
+}
